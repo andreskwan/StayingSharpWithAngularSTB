405c8bd6861ca8c4c1b0578cdc80dd3e93158fe1
ANDRES KWAN <andreskwan@gmail.com>
2015-05-07 07:26:19 -0500

    Note-Edit - Disable submit while updating data

--- app/assets/javascript/controllers/noteEditController.js
+++ app/assets/javascript/controllers/noteEditController.js
@@ -6,7 +6,12 @@
 angular.module('NoteWrangler')
 .controller('NotesEditController', function(Note, $scope, $routeParams){
 	$scope.note = Note.get({id: $routeParams.id});
+	$scope.isSubmittting = false;
+
 	$scope.saveNote = function(note){
-		note.$update();
+		$scope.isSubmittting = true;
+		note.$update().finally(function(){
+			$scope.isSubmittting = false;
+		});
 	};
 });
