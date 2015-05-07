1f477b27a5225b2e5402bb7ea554ace6aa233857
ANDRES KWAN <andreskwan@gmail.com>
2015-05-07 07:30:48 -0500

    Note-Edit-Show - return to note -show after edition complete

--- app/assets/javascript/controllers/noteEditController.js
+++ app/assets/javascript/controllers/noteEditController.js
@@ -4,14 +4,17 @@
 * noteEditController
 */
 angular.module('NoteWrangler')
-.controller('NotesEditController', function(Note, $scope, $routeParams){
+.controller('NotesEditController', function(Note, $scope, $routeParams, $location){
 	$scope.note = Note.get({id: $routeParams.id});
 	$scope.isSubmittting = false;
 
 	$scope.saveNote = function(note){
 		$scope.isSubmittting = true;
+
 		note.$update().finally(function(){
 			$scope.isSubmittting = false;
+			$location.path("/notes/"+note.id);
+
 		});
 	};
 });
