0e1b514d7db6ef6608cb149952107ececb30c5fa
ANDRES KWAN <andreskwan@gmail.com>
2015-05-07 23:02:14 -0500

    Note-Delete - complete

--- app/assets/javascript/controllers/noteShowController.js
+++ app/assets/javascript/controllers/noteShowController.js
@@ -4,7 +4,23 @@
 * noteShowController
 */
 angular.module('NoteWrangler')
-.controller('NotesShowController', function(Note, $scope, $routeParams){
+.controller('NotesShowController', function(Note, $scope, $routeParams, $location){
 	$scope.note = Note.get({id: $routeParams.id});
-	console.log($scope.note);
+	$scope.deleteNote = function(note){
+		note.$remove().then(function(){
+			$location.path("/notes/");
+		});
+	};
 });
+
+// Directives
+
+// Markers on a **DOM element** that tell AngularJS's HTML compiler `$compile` to attach a specified behavior to that DOM element.
+
+// Markers on a **DOM element**
+
+// What is a Service
+
+// Define Service
+
+// Service: Angular services are objects that are wired together using dependency injection (DI). You can use services to organize and share code across your app.
