0cd808fa1b74d27f6330aa176d1dd7d50f4ec8a1
ANDRES KWAN <andreskwan@gmail.com>
2015-05-10 10:06:42 -0500

    Note-Create - after creating retur to show note - by using the id of the note

--- app/assets/javascript/controllers/noteCreateController.js
+++ app/assets/javascript/controllers/noteCreateController.js
@@ -4,6 +4,18 @@
 * noteIndexController
 */
 angular.module('NoteWrangler')
-.controller('NotesCreateController', [function(){
-	
-}]);
+.controller('NotesCreateController', function(Note,$scope,$location){
+	//new resource 
+	$scope.note = new Note();
+	$scope.isSubmitting = false;
+
+	$scope.saveNote = function(note){
+		$scope.isSubmitting = true;
+		note.$save().then(function(note){
+			// console.log("------this is the note returned from server: ",note);
+			$location.path("/notes/"+note.id);
+		}).finally(function(){
+			$scope.isSubmitting = false;
+		});
+	};
+});
