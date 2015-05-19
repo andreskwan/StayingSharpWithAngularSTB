11902f67fd79fc9cdd640a780773ae6efe5eafc5
ANDRES KWAN <andreskwan@gmail.com>
2015-05-19 06:05:07 -0500

    Directive - navbar - working -selects menu item acording to page path

--- app/assets/javascript/directives/nwPageNav.js
+++ app/assets/javascript/directives/nwPageNav.js
@@ -6,13 +6,24 @@
 angular.module('NoteWrangler')
 .directive('nwPageNav', function(){
 	return {
-		restrict: 'E',
-		templateUrl: 'assets/templates/directives/nwPageNav.html',
 		replace: true,
-		controller: function(){
-			$scope.isPage = function($scope){
-				
-			}
+		restrict: 'E',			 
+		templateUrl: 'assets/templates/directives/nwPageNav.html',
+		controller: function ($scope, $location){
+			//what is name?? who delivers this value?
+			$scope.isPage = function(name){
+				//using regex to identify the page where we are to enable the nav
+				//find $ or / at the end
+				// debugger;
+				// console.log("location.path(): ",$location.path());
+				//    /\/notes($|\/)/
+				// this expression returns a boolean
+				var pageName = new RegExp(name).test($location.path());
+				console.log("location.path():",$location.path());
+				console.log("name",name);
+				return pageName;
+				// return false;
+			};
 		}
 	};
 });
